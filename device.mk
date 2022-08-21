80fc41d9ccf83ca40792341c49f548b641f786ef
MaiconHenrique93 <107500583+MaiconHenrique93@users.noreply.github.com>
2022-08-21 20:27:47 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -211,7 +211,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libgui_vendor \
-    Snap \
+    MotCamera3 \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Common init scripts
